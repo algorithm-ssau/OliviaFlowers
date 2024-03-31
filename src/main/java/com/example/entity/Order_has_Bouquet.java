45fa922dcf89374220937ed948a49bc0e9fb0e61
AntonSTT <woroprod@gmail.com>
2024-03-31 21:35:07 +0400

    Добавлены аттрибуты в связующую сущность "Заказ-Букет"

--- src/main/java/com/example/entity/Order_has_Bouquet.java
+++ src/main/java/com/example/entity/Order_has_Bouquet.java
@@ -1,4 +1,8 @@
 package com.example.entity;
 
 public class Order_has_Bouquet {
+    private Long idOrder; //id Заказа
+    private Long idBouquet; //id Букета
+
+    private Long count; //Количество цветов
 }
