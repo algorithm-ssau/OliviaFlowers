7062199cf91290cffd0621ba5747fd98982b440a
AntonSTT <woroprod@gmail.com>
2024-03-31 21:02:44 +0400

    Добавлены атрибуты в сущность "Заказ"

--- src/main/java/com/example/entity/Order.java
+++ src/main/java/com/example/entity/Order.java
@@ -1,10 +1,22 @@
 package com.example.entity;
 
+import java.time.LocalDateTime;
+
 public class Order {
-    private Long id;
-    private Long userId;
-    private Long typePostcard;
-    private String textPostcard;
+    private Long id;  //id заказа
+    private Long userId; //id пользователя
+    private Long typePostcard; //тип открытки
+    private String textPostcard; //текст открытки
+
+    private Boolean toDeliver; //доставка или самовывоз
+    private String address; //Адрес доставки заказа
+
+    private LocalDateTime dateTimeDelivery; //время доставки
+
+    private Long sumOrderl; //сумма заказа
+
+    private LocalDateTime datePayment; //время оплаты заказа
+
 
 
 }
