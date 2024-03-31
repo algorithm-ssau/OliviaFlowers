ad13be3f762c8843c28500fab4409ff556ab9360
GrishkoVi <grishkovavi7@mail.ru>
2024-03-31 23:16:22 +0400

    Добавление атрибута в сущность "Товар"

--- src/main/java/com/example/entity/Bouquet.java
+++ src/main/java/com/example/entity/Bouquet.java
@@ -1,4 +1,20 @@
 package com.example.entity;
 
+import jakarta.persistence.*;
+import jdk.jfr.Name;
+import lombok.Data;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
 public class Bouquet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id; //id цветка
+
+
+
 }
