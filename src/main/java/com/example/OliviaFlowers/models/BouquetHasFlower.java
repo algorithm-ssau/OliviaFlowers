a8769c1157b7c11723874bb76f598750b4629d77
Polina24zinoveva <polina.zinoveva@list.ru>
2024-04-21 11:43:13 +0400

    Добавила аннотации

--- src/main/java/com/example/OliviaFlowers/models/BouquetHasFlower.java
+++ src/main/java/com/example/OliviaFlowers/models/BouquetHasFlower.java
@@ -1,8 +1,16 @@
 package com.example.OliviaFlowers.models;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class BouquetHasFlower {
     @Id
     private Long idOrderidBouquet; //id букета
