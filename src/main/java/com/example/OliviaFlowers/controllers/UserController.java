c511fa976d6f6d693e2bd316b20a652cb9c07a7e
Lumer1337 <ilyapleshakov2003@mail.ru>
2024-04-22 17:34:45 +0400

    Добавление User и UserService

--- src/main/java/com/example/OliviaFlowers/controllers/UserController.java
+++ src/main/java/com/example/OliviaFlowers/controllers/UserController.java
@@ -1,9 +1,18 @@
 package com.example.OliviaFlowers.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+
+    @GetMapping("/registration")
+    public String registration(){
+        return "registration";
+    }
+
+
     @GetMapping("/user")
     public String user(){
         return "user";
