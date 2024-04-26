2f5d4d31ee2acce3923a0ecb7afdc2bf8346d4ae
AntonSTT <woroprod@gmail.com>
2024-04-26 20:33:18 +0400

    Добавление репозитория, изменение сервиса для последующей реализации функционала админской страницы

--- src/main/java/com/example/OliviaFlowers/secvices/BouquetService.java
+++ src/main/java/com/example/OliviaFlowers/secvices/BouquetService.java
@@ -1,7 +1,35 @@
 package com.example.OliviaFlowers.secvices;
 
+import com.example.OliviaFlowers.models.Bouquet;
+import com.example.OliviaFlowers.repositories.BouquetRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class BouquetService {
+    private final BouquetRepository bouquetRepository;
+
+    public List<Bouquet> listAllBouquets(){
+
+        return bouquetRepository.findAll();
+    }
+
+    public void saveBouquet(Bouquet bouquet){
+        log.info("Creating new {}", bouquet);
+        bouquetRepository.save(bouquet);
+    }
+
+    public void deleteBouquet(Long id){
+        bouquetRepository.deleteById(id);
+    }
+
+    public Bouquet getBouquetByID(Long id){
+        return bouquetRepository.findById(id).orElse(null);
+    }
 }
