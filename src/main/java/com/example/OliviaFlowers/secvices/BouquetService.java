7e0fac1c3b0c7fe5e4c79474915a6b20e528fa58
GrishkoVi <grishkovavi7@mail.ru>
2024-04-27 19:31:06 +0400

    Изменение контроллера

--- src/main/java/com/example/OliviaFlowers/secvices/BouquetService.java
+++ src/main/java/com/example/OliviaFlowers/secvices/BouquetService.java
@@ -13,4 +13,6 @@ public class BouquetService {
         bouquets.add(new Bouquet(Long.valueOf(1),"Букет","",Long.valueOf(2000) ));
         bouquets.add(new Bouquet(Long.valueOf(2),"Букет","",Long.valueOf(3000) ));
     }
+
+    public List<Bouquet> listBouquets() {return bouquets;}
 }
