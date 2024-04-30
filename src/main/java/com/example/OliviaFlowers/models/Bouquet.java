a56fccbf9068ad1af4d1162e7771e5a740dfa001
Polina24zinoveva <polina.zinoveva@list.ru>
2024-04-30 21:39:00 +0400

    Организация связи с таблицей Фотографии

--- src/main/java/com/example/OliviaFlowers/models/Bouquet.java
+++ src/main/java/com/example/OliviaFlowers/models/Bouquet.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -15,15 +16,19 @@ import java.util.List;
 public class Bouquet {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; //id цветка
+    private Long id; //id букета
 
     @Column(name = "name")
     private String name; //название
 
-    @Column(name = "photo")
-    private String photo; //фотография
+    @Column(name = "composition", columnDefinition = "text")
+    private String composition;
 
     @Column(name = "price")
     private Long price; //цена
+
+    @OneToMany
+    private List<Image> images = new ArrayList<>();
+    private Long previewImageID;
 
 }
