789bfa987e499b2c1bd6ee4d8df2dfd64b7f133b
Polina24zinoveva <polina.zinoveva@list.ru>
2024-04-30 21:38:36 +0400

    Добавление таблицы Фотографии

---
+++ src/main/java/com/example/OliviaFlowers/models/Image.java
@@ -0,0 +1,39 @@
+package com.example.OliviaFlowers.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String name;
+
+    @Column
+    private String originalFileName;
+
+    @Column
+    private Long size;
+
+    @Column
+    private String contentType;
+
+    @Column
+    private boolean isPreviewImage;
+
+    @Column(name = "photo", columnDefinition = "LONGBLOB")
+    @Lob
+    private byte[] bytes;
+
+    @ManyToOne
+    private Bouquet bouquet;
+}
