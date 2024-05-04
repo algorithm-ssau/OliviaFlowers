b9ec1f6a02f0986321071cb9662457d11b3e229e
AntonSTT <woroprod@gmail.com>
2024-05-04 15:47:42 +0400

    Добавление основных элементов для функционирования корзины (Order)

--- src/main/java/com/example/OliviaFlowers/controllers/OrderController.java
+++ src/main/java/com/example/OliviaFlowers/controllers/OrderController.java
@@ -1,9 +1,14 @@
 package com.example.OliviaFlowers.controllers;
 
+import com.example.OliviaFlowers.models.Order;
 import com.example.OliviaFlowers.secvices.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.security.Principal;
+
 @Controller
 public class OrderController {
     @Autowired
@@ -17,6 +22,14 @@ public class OrderController {
     public String order(){
         return "order";
     }
+
+    @GetMapping("/order/add")
+    public String addOrder(Order order, Principal principal) throws IOException {
+        orderService.saveOrder(principal, order);
+        return "redirect:/order";
+    }
+
+
 
 
 }
