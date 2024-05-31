bd9d502176624aedf4959e7ba12f1a72c7154e66
Polina24zinoveva <polina.zinoveva@list.ru>
2024-05-31 16:06:38 +0400

    Добавила уведомление, при неверном логине или регистрации

--- src/main/java/com/example/OliviaFlowers/controllers/UserController.java
+++ src/main/java/com/example/OliviaFlowers/controllers/UserController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.LocalDate;
+
 @Controller
 
 public class UserController {
@@ -19,6 +21,8 @@ public class UserController {
     private final UserService userService;
 
     private String message = null;
+    private String warning = null;
+    private String error = null;
 
 
     public UserController(UserService userService) {
@@ -27,18 +31,33 @@ public class UserController {
 
     @GetMapping("/login")
     public String login(Model model) {
+        LocalDate maxDate = LocalDate.now();
+        model.addAttribute("maxDate", maxDate);
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
         User user = userService.getUserByEmail(username);
         model.addAttribute("isAdmin", user != null && user.getIsAdministrator());
         if (message != null) model.addAttribute("message", message);
+        if (warning != null) model.addAttribute("warning", warning);
+        if (error != null) model.addAttribute("error", error);
         message = null;
+        warning = null;
+        error = null;
 
         return "login";
     }
 
+    @GetMapping("/login_not_success")
+    public String login_not_success() {
+        error = "Не верный email или пароль";
+        return "redirect:/login";
+    }
+
     @GetMapping("/registration")
     public String registration(Model model){
+
+
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
         User user = userService.getUserByEmail(username);
@@ -54,11 +73,16 @@ public class UserController {
 
     @PostMapping("/registration")
     public String createUser(UserWithoutLink userWithoutLink, Model model) {
-        message = "На вашу электронную почту отправлен код для активации аккаунта";
-        if (!userService.createUser(userWithoutLink)) {
-            return "redirect:/login?error";
+        User user = userService.getUserByEmail(userWithoutLink.getEmail());
+        if (user != null) {
+            warning = "На этот email уже зарегистрирован аккаунт";
         }
-
+        else {
+            message = "На вашу электронную почту отправлен код для активации аккаунта";
+            if (!userService.createUser(userWithoutLink)) {
+                return "redirect:/login?error";
+            }
+        }
         return "redirect:/login";
     }
 
